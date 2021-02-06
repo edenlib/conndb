c02304798d890c1c7981bb9f2a14311eef43106e
Adam Turner <turner.adch@gmail.com>
2021-02-06 10:41:12 -0500

    packaged according to PyPA tutorial

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # conndb
 Connect to a database.
 ```shell
-$ pip install --upgrade
-
+$ pip install --upgrade git+https://github.com/edenlib/conndb.git#egg=conndb
 ```
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="conndb",  # this is what you pip install 'pip install name'
-    version="0.0.1",
-    description="Connect to a database.",
-    py_modules=["conndb"],
-    package_dir={"": "src"}
-)
